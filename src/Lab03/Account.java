eff83ad46e775d4679ec044fe4c39e0210b7ca41
Guilherme Balabanian Mascaretti <guimascaretti@gmail.com>
2025-09-04 15:18:05 -0300

    lab3/4 - senha, saque e deposito

--- src/Lab03/Account.java
+++ src/Lab03/Account.java
@@ -6,12 +6,65 @@ public class Account {
     private double balance;
     private int num;
     private double limit;
+    private String senha;
 
 
+    public Account(){
+        this.name = "";
+        this.num = 0;
+        this.limit = 0.0;
+        this.balance = 0.0;
+        this.senha = "0000";
+    }
 
+    public Account(String name, int num, double limit){
+        this.name = name;
+        this.num = num;
+        this.limit = limit;
+        this.balance = 0.0;
+        this.senha = "0000";
+    }
 
-    public void setBalance(double balance) {
-        this.balance = balance;
+    public void deposit(double value){
+        if(value > 0)
+        {
+            this.balance += value;
+            System.out.println("Depósito realizado:" + balance);
+        }
+        else{
+            System.out.println("Não houve depósito");
+        }
+    }
+
+    public void withdraw(double value){
+        if(value<=0)
+        {
+            System.out.println("Valor invalido");
+            return;
+        }
+
+        if(balance > 0 && value <= limit)
+        {
+            balance -= value;
+            System.out.println("Saque realizado com sucesso: " + balance);
+        }
+        else{
+            System.out.println("Ausência de saldo ou estouro de limite");
+        }
+
+    }
+
+    public void MudarSenha(String senhaAnt, String senhaNova)
+    {
+        if(this.senha.equals(senhaAnt))
+        {
+            this.senha = senhaNova;
+            System.out.println("Senha alterada!");
+        }
+        else
+        {
+            System.out.println("Impossivel alterar a senha, senha incorreta!");
+        }
     }
 
     public double getBalance() {
--- src/Lab03/AccountTest.java
+++ src/Lab03/AccountTest.java
@@ -10,22 +10,32 @@ public static void main(String[] args){
     Account conta3 = new Account();
 
     conta1.setName("Paulo");
-    conta1.setBalance(355.78);
     conta1.setLimit(10000);
     conta1.setNum(01);
     conta1.imprime();
+    conta1.deposit(500);
+    conta1.withdraw(252);
+    conta1.MudarSenha("0000","6969");
+
 
     conta2.setName("Marlene");
-    conta2.setBalance(333.33);
     conta2.setLimit(8000);
     conta2.setNum(02);
     conta2.imprime();
+    conta2.deposit(777);
+    conta2.withdraw(111);
+    conta2.MudarSenha("0001","7654");
+
 
     conta3.setName("Jeremias");
-    conta3.setBalance(233.19);
     conta3.setLimit(1000);
     conta3.setNum(03);
     conta3.imprime();
+    conta3.deposit(111.99);
+    conta3.withdraw(2000);
+    conta3.MudarSenha("0000","1222");
+
+
 
 
 
